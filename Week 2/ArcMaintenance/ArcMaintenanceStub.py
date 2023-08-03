95976309c63e81782922961ccb8a506aff306660
Hugo Burton <burto732@gmail.com>
2023-08-03 10:21:00 +1000

    Initial Setup for Bender's Decomposition Implemented BSP and BMP

--- Week 2/ArcMaintenance/ArcMaintenanceStub.py
+++ Week 2/ArcMaintenance/ArcMaintenanceStub.py
@@ -1,4 +1,4 @@
-from gurobipy import *
+from gurobipy import Model, quicksum, GRB
 
 # Initialise empty data
 Nodes = []
@@ -51,3 +51,33 @@ JobsTA = [[[(j,p) for j in JobsA[a] for p in JP[j] if p<=t and p+Jobs[j][3]>t]
            for a in A] for t in T]
 print('Calculated JobsTA')
 
+BSP = Model()
+BSP.setParam('OutputFlag', 0)
+
+X = {a: BSP.addVar() for a in A}
+
+BSP.setObjective(X[BackArc], GRB.MAXIMIZE)
+
+FlowConversion = {n: BSP.addConstr(quicksum(X[a] for a in A if Arcs[a][0] == n) ==
+                                 quicksum(X[a] for a in A if Arcs[a][1]==n)) for n in N}
+
+
+UpperBound = {a: BSP.addConstr(X[a]<=Arcs[a][2])
+              for a in X}
+
+BSP.optimize()
+print(BSP.objVal)
+
+
+
+BMP = Model()
+
+Y = {(j,t): BMP.addVar(vtype=GRB.BINARY) for j in J for t in JP[j]}
+Theta = {t: BMP.addVar(ub=BSP.objVal) for t in T}
+BMP.setObjective(quicksum(Theta[t] for t in T), GRB.MAXIMIZE)
+
+
+EachJobOnce = {j: BMP.addConstr(quicksum(Y[j,t] for t in JP[j])==1)
+                                for j in J}
+
+BMP.optimize()
