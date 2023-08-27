619f77f13fcdc529980e373fd920a8dd2f253683
Hugo Burton <burto732@gmail.com>
2023-08-27 18:12:35 +1000

    Constraint Class

---
+++ Project/Constraint.py
@@ -0,0 +1,52 @@
+"""
+Class for handing constraints (hard and soft) in the problem
+"""
+
+
+class Constraint:
+    def __init__(self, constraint_data):
+        self.level = constraint_data.get("Level")
+        self.period = constraint_data.get("Period")
+        self.room = constraint_data.get("Room")
+        self.type = constraint_data.get("Type")
+        self.course = constraint_data.get("Course")
+        self.exam = constraint_data.get("Exam")
+        self.part = constraint_data.get("Part")
+
+    def __repr__(self):
+        if self.type == "RoomPeriodConstraint":
+            return f"Forbidden Room {self.room} at Period {self.period}"
+        elif self.type == "EventPeriodConstraint":
+            if self.part:
+                return f"{self.level} {self.course} Exam {self.exam} - {self.part} at Period {self.period}"
+            else:
+                return f"{self.level} {self.course} Exam {self.exam} at Period {self.period}"
+        elif self.type == "PeriodConstraint":
+            return f"{self.level} at Period {self.period}"
+
+
+class ConstraintManager:
+    def __init__(self):
+        self.constraints = []
+
+    def add_constraint(self, constraint_data):
+        new_constraint = Constraint(constraint_data)
+        self.constraints.append(new_constraint)
+
+
+# Example data
+data = {
+    "Constraints": [
+        # ... (constraints data)
+    ]
+}
+
+constraint_manager = ConstraintManager()
+
+# Add constraints to the ConstraintManager
+for constraint_data in data["Constraints"]:
+    constraint_manager.add_constraint(constraint_data)
+
+# Print constraints using their __repr__ method
+for constraint in constraint_manager.constraints:
+    print(constraint)
