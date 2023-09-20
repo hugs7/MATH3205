c502165a8d77427936cf9bbb20754cb10de49955
hugs7 <burto732@gmail.com>
2023-09-20 22:29:00 +1000

    Defined Constants file

---
+++ Project/Constants.py
@@ -0,0 +1,26 @@
+COURSE = "Course"
+NUMBER_OF_EXAMS = "NumberOfExams"
+
+# Exam types
+EXAM_TYPE = "ExamType"
+WRITTEN_AND_ORAL = "WrittenAndOral"
+ORAL = "Oral"
+WRITTEN = "Written"
+WRITTEN_ORAL_SPECS = "WrittenOralSpecs"
+
+MINIMUM_DISTANCE_BETWEEN_EXAMS = "MinimumDistanceBetweenExams"
+
+ROOMS_REQUESTED = "RoomsRequested"
+
+TEACHER = "Teacher"
+
+# Rooms Requested
+NUMBER = "Number"
+TYPE = "Type"
+
+
+# Written Oral Specs
+MAX_DISTANCE = "MaxDistance"
+MIN_DISTANCE = "MinDistance"
+ROOM_FOR_ORAL = "RoomForOral"
+SAME_DAY = "SameDay"
