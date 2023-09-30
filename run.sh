00100f884602e61735235fa5905c96627e36b886
Mitchell Holt <mitchellholt2002@gmail.com>
2023-09-30 15:36:34 +1000

    bug fixes for dummy room; removed * imports from Course; small improvements in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python3 Project/NaiveImplementation.py | tee out
-echo "\n\n" >> out
-examtt_toolbox validate-solution Project/data/toy.json Project/toy_sol.json 2>> out
+echo "\n---------- Solution Checker ----------" >> out
+examtt_toolbox validate-solution Project/data/toy.json Project/toy_sol.json 2>&1 | tee -a out
